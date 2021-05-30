6c5a9fd1d6569f0fff63721c7ba67b98a1e5d33e
Joshua <jtjackson325@gmail.com>
2021-05-30 16:10:43 -0600

    added mouseenter event to give colors to timeblocks

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -3,4 +3,17 @@ var currentDate = moment();
 
 todaysDate.textContent = currentDate.format("dddd DD, YYYY");
 
+$(".row").mouseenter(function(){
+    var time = $(this).text().trim();
+
+    var hour = moment(time, "L").set("hour", 12);
+
+    if (moment().isAfter(hour)) {
+        $(this).find("textarea").addClass("future");
+    } else if (moment().isBefore(hour)) {
+        $(this).find("textarea").addClass("past")
+    } else {
+        $(this).find("textarea").addClass("present")
+    }
+})
 
